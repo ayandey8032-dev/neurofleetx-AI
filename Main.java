e20430fb1d086f115ef0ecfae5fbb24dfa5ecf48
ayandey8032-dev <ayandey8032@gmail.com>
2025-08-19 16:04:18 +0530

    Main

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+class Car {
+    
+    String brand;
+    int year;
+
+    
+    void displayDetails() {
+        System.out.println("Car Brand: " + brand);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Car myCar = new Car();
+
+    
+        myCar.brand = "Tesla";
+        myCar.year = 2023;
+
+        
+        myCar.displayDetails();
+    }
+}
